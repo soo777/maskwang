cca7f492b2f7ddf2356edfae4e269fdf5923dc8a
soo777 <ssm19944@gmail.com>
2020-04-09 17:36:07 +0900

    test mobx sample store

--- maskwang/src/App.tsx
+++ maskwang/src/App.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import Sample from "~/sample/Sample";
+import SampleStore from "~/sample/SampleStore";
+import {Provider} from "mobx-react";
+
+const sampleStore = new SampleStore();
 
 function App() {
   return (
-    <Sample/>
+      <Provider
+          sampleStore = {sampleStore}
+      >
+        <Sample/>
+      </Provider>
   );
 }
 
--- maskwang/src/sample/Sample.tsx
+++ maskwang/src/sample/Sample.tsx
@@ -1,16 +1,30 @@
-import * as React from 'react';
-import {observer} from "mobx-react";
+import React from 'react';
+import {inject, observer} from "mobx-react";
+import SampleStore from "~/sample/SampleStore";
+import autoBind from "auto-bind";
 
 interface Props{
-
+    sampleStore?:SampleStore;
 }
 
+@inject("sampleStore")
 @observer
 class Sample extends React.Component<Props> {
 
+    constructor(props: any){
+        super(props);
+        autoBind(this);
+    }
+
+    componentDidMount() {
+        this.props.sampleStore?.find();
+    }
+
     render() {
+        const {sampleStore} = this.props;
+
         return (
-            <div>sample</div>
+            <div>sample ----- {sampleStore!.name}</div>
         )
     }
 }
