c1bee0c3e0e8df3376bbe9781fd0734072e8042c
soo777 <ssm19944@gmail.com>
2020-04-23 23:53:21 +0900

    handle search Input

--- maskwang/src/component/container/SearchBoxContainer.tsx
+++ maskwang/src/component/container/SearchBoxContainer.tsx
@@ -1,13 +1,24 @@
 import React from 'react';
 import {Button, Input} from "semantic-ui-react";
+import {inject, observer} from "mobx-react";
+import SearchStore from "~/component/store/SearchStore";
 
+interface Props{
+    searchStore?:SearchStore;
+}
 
-class SearchBoxContainer extends React.Component{
+@inject('searchStore')
+@observer
+class SearchBoxContainer extends React.Component<Props>{
+
+        handleChange = (e:any) =>{
+            this.props.searchStore?.handleInput(e.target.value);
+        };
 
         render(){
             const searchDiv = {
                 background: 'steelblue'
-            }
+            };
 
             const boxStyle = {
                 marginLeft:'40%',
@@ -16,13 +27,13 @@ class SearchBoxContainer extends React.Component{
 
             const searchBtn = {
                 marginLeft: '10px'
-            }
+            };
 
             return(
                 <div style={searchDiv}>
                     <div style={boxStyle}>
-                        <Input focus placeholder='Search...' />
-                        <Button style={searchBtn}>Search</Button>
+                        <Input focus placeholder='Search...' onChange={this.handleChange} />
+                        {/*<Button style={searchBtn}>Search</Button>*/}
                     </div>
                 </div>
             )
---
+++ maskwang/src/component/store/SearchStore.ts
@@ -0,0 +1,16 @@
+import autoBind from 'auto-bind';
+import {action, observable} from 'mobx';
+
+class SearchStore{
+
+    @observable
+    searchInput:string = '';
+
+    @action
+    handleInput(input:string){
+        this.searchInput = input;
+        console.log(this.searchInput);
+    }
+}
+
+export default SearchStore;
