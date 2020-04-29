a9f1e27eccbcd127d50f77c56d5d38c19fa09145
soo777 <ssm19944@gmail.com>
2020-04-29 22:49:17 +0900

    add markerInfo

--- maskwang/src/component/container/MapContainer.tsx
+++ maskwang/src/component/container/MapContainer.tsx
@@ -2,6 +2,11 @@ import React from 'react';
 import {inject, observer} from "mobx-react";
 import autoBind from "auto-bind";
 import MapStore from "~/component/store/MapStore";
+import marker_green from '../../../public/images/marker_green.png';
+import marker_yellow from '../../../public/images/marker_yellow.png';
+import marker_red from '../../../public/images/marker_red.png';
+import marker_grey from '../../../public/images/marker_grey.png';
+import marker_black from '../../../public/images/marker_black.png';
 
 declare global {
     interface Window {
@@ -100,9 +105,28 @@ class MapContainer extends React.Component<Props> {
     }
 
     render(){
+        const markerInfo = {
+            position:'absolute' as 'absolute',
+            right:'10px',
+            bottom:'10px',
+            zIndex:1,
+            fontSize:'15px'
+        };
+
+        const image ={
+            width:'25px'
+        }
+
         return(
             <div>
                 <div id="map" style={{width:'100vw', height:'94vh'}}></div>
+                <div style={markerInfo}>
+                    <div><img src={marker_green} style={image}/>100개 이상</div>
+                    <div><img src={marker_yellow} style={image}/>30~100개</div>
+                    <div><img src={marker_red} style={image}/>30개 미만</div>
+                    <div><img src={marker_grey} style={image}/>1개 이하</div>
+                    <div><img src={marker_black} style={image}/>판매 중지</div>
+                </div>
             </div>
         )
     }
